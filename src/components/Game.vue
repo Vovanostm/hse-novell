67963fd52a84ebac2b86b1caff3bf19d14edc125
vovanostm <axb48@yandex.ru>
2018-04-15 12:39:28 +0300

    initial commit

---
+++ src/components/Game.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div
+        v-if="room==='Begin'"
+    >
+        <begin></begin>
+    </div>
+  </div>
+</template>
+<script>
+import Begin from './rooms/Begin'
+export default {
+  name:'Game',
+  components: {
+      Begin
+  },
+  data () {
+      return{
+        room:'Begin'
+      }
+  }
+}
+</script>
+
---
+++ src/components/rooms/Begin.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <img src="http://tsarselo.ru/images/photos/23755483e23e51e0e5a0755e29434dd5.jpg" alt="">
+      <h2>Скоро экзамены</h2>
+      <p>Ваши действия</p>
+      <button>Подготовиться и сдать на 100 баллов</button>
+      <button>Ездить на олимпиады</button>
+      <button>Играть в дотку</button>
+  </div>
+</template>
